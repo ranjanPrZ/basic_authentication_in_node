f1817db017eb4052f1f9f070de0fbae3be749561
RANJAN <spak@RANJANs-MacBook-Pro.local>
2022-07-20 13:19:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const passport = require('passport');
+const app = express();
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+app.use(express.static(__dirname));
+
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ' + port));
+
+
+
+// -------------------
+mongoose.connect('mongodb://localhost/MyDatabase',
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+  username: String,
+  password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
